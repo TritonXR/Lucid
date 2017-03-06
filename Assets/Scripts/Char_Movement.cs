b8c04f29d73a733583a3ae106654a8172b1ebe54
aguazul <aguazul@gmail.com>
2017-03-05 18:35:09 -0800

    Build 3-5-2017
    Added omnidirectional movement, increased FPS, added Dreamer Giaus
    model, added controller model

---
+++ Assets/Scripts/Char_Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Char_Movement : MonoBehaviour
+{
+    // How fast to move
+    public float speed = 7.0F;
+    // CharacterController script
+    private CharacterController controller;
+    // GvrViewer Script
+    private GvrViewer gvrViewer;
+    // VR Head
+    private Transform vrHead;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Find the CharacterController
+        controller = GetComponent<CharacterController>();
+        // Find the GvrViewer on child 0
+        gvrViewer = transform.GetChild(0).GetComponent<GvrViewer>();
+        // Fnd the VR Head
+        vrHead = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 touchPos;
+        float touchX, touchY;
+
+        // In the Google VR button press
+        if (GvrController.IsTouching)
+        {
+            touchPos = GvrController.TouchPos;
+
+            Vector3 forward = vrHead.TransformDirection(Vector3.forward);
+            Vector3 right = vrHead.TransformDirection(Vector3.right);
+
+            touchX = touchPos.x - 0.5F;
+            touchY = touchPos.y - 0.5F;
+
+            controller.SimpleMove(forward * speed * touchY * -1);
+            controller.SimpleMove(right * speed * touchX);
+        }
+    }
+}
