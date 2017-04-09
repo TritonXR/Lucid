9064689c2511b555c8ba305f517cf6a342721a21
aguazul <aguazul@gmail.com>
2017-04-08 17:56:38 -0700

    Test Commit with fixed movement
    PLEASE LET THIS WORK!!!!

---
+++ Assets/Scripts/LoadMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour {
+
+    public Canvas startmenu;
+    public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+
+        startButton = startButton.GetComponent<Button>();
+        //optionButton = exitButton.GetComponent<Button>();
+        //aboutButton = aboutButton.GetComponent<Button>();
+       // exitButton = exitButton.GetComponent<Button>();
+
+
+	}
+
+    //Start Button
+    public void StartButtonPress()
+    {
+
+        SceneManager.LoadScene("Hallway_10");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
