e37d6029e06e6ea8450d52477e7ffc5883bceb7f
Yunchu He <yuh134@ucsd.edu>
2017-06-03 11:52:56 -0700

    HUD

---
+++ Assets/Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+public RectTransform rectransform;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 player3DPosition = Camera.main.WorldToScreenPoint (transform.position);
+		rectransform.position = player3DPosition;
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float Hp;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localPosition = new Vector3 ((-(1-(Hp/100))*150)/1000,0.0f,0.0f);
+		this.transform.localScale = new Vector3 (Hp/100, 1, 1);
+	}
+}
